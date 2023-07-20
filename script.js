5c74f9439fcbc3c118b2eb82fde69af1d8bdb09a
Nogiback <peter.toan.do@gmail.com>
2023-07-20 18:22:25 -0400

    Add display and number buttons, display is now showing numbers. Change styling on most html elements. Add footer

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-let num1 = null;
-let num2 = null;
-let operator = null;
+let num1 = '';
+let num2 = '';
+let currentOperator = null;
+let resetFlag = false;
 
 const display = document.querySelector('.display');
+const numberButtons = document.querySelectorAll('.num-button');
+const operatorButtons = document.querySelectorAll('.op-button');
+const equalsButton = document.getElementById('equals');
+const clearButton = document.getElementById('clear');
 
+//equalsButton.addEventListener('click', checkOperation);
+clearButton.addEventListener('click', clearAll);
+
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+);
+
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', () => getOperation(button.textContent))
+);
+
+function appendNumber(num) {
+  if (display.textContent === '0' || resetFlag) {
+    resetDisplay();
+  }
+  display.textContent += num;
+  console.log(num);
+}
+
+function resetDisplay() {
+  display.textContent = '';
+  resetFlag = false;
+}
+
+function clearAll() {
+  display.textContent = '0';
+  num1 = '';
+  num2 = '';
+  currentOperator = null;
+}
 
 function add (num1, num2) {
   return num1 + num2;
@@ -21,22 +56,26 @@ function divide (num1, num2) {
   return num1 / num2;
 }
 
-function operate(operator, num1, num2) {
-  let answer = null;
-  if (operator === '+') {
-    answer = add(num1, num2);
-  } else if (operator === '-') {
-    answer = subtract(num1, num2);
-  } else if (operator === '*') {
-    answer = multiply(num1, num2);
-  } else if (operator === `/`) {
-    answer = divide(num1, num2);
-  } else {
-    alert("Invalid operator");
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a,b);
+    case '×':
+      return multiply(a,b);
+    case '÷':
+      if (b===0) {
+        return null
+      } else {
+        return divide(a,b);
+      }
+    default:
+      return null;  
   }
-
-  alert(answer);
-
 }
 
 //testing if functions working as intended
